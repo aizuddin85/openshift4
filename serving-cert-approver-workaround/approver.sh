ede9a9ea5a0ecb0c956dfee2c08ad600177ae040
Muhammad Aizuddin <mzali@redhat.com>
2020-11-24 01:10:31 +0800

    Update approver.sh
    Cleanup unnecessary logic.

--- serving-cert-approver-workaround/approver.sh
+++ serving-cert-approver-workaround/approver.sh
@@ -1,14 +1,10 @@
 echo "Logged-in as: `oc whoami`"
 
-csrcount=`oc get csr | grep -i Pending |wc -l`
 pendingcsr=`oc get csr | grep -i Pending  | awk '{print $1}'`
 
-if [ $csrcount -gt 0 ]
+if [ $pendingcsr -gt 0 ]
 then
-  echo "Listing all Pending CSRs..."
-  oc get csr | grep Pending
   echo "Approving only Node Serving Certificate CSR, please approve node-bootstrapper manually..."
-
   for csr in $pendingcsr; do
     if [[ `oc get csr  -o  custom-columns=:.spec.username $csr` == *"node-bootstrapper"* ]]; then
   	  echo "Detected $csr as bootstrap request, skipping, please approve manually ..."
