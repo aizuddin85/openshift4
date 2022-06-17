d601663128dead3034a35f33164d2ecbda86adfa
Muhammad Aizuddin <aizuddin.zali@gmail.com>
2022-06-17 17:56:26 +0800

    update script based

--- aws/lambda/start-ec2.py
+++ aws/lambda/start-ec2.py
@@ -1,35 +1,43 @@
-#!/usr/bin/python3
-
+import json
 import boto3
 
-openshift_tag = "tag:kubernetes.io/cluster/$cluster_id_here"
+openshift_tag = "tag:kubernetes.io/cluster/XXXXXXXX"
+region = "ap-southeast-1"
 state = "stopped"
-region = "ap-southeast=1"
-bootup = False
+bootup = True
 ec2_to_bootup = []
 
-session =  boto3.Session(region_name=region)
-
-ec2 = session.resource('ec2', region)
-
-instances = ec2.instances.filter(
+def lambda_handler(event, context):
+    session = boto3.Session(region_name=region)
+    
+    ec2 = session.resource('ec2', region)
+    
+    instances = ec2.instances.filter(
           Filters=[
             {'Name': 'instance-state-name', 
              'Values': [state]},
             {'Name': openshift_tag,
              'Values': ['owned']}]
             )
-print("Listing all stopped instances for %s tag..." % openshift_tag) 
-
-for ec2 in instances:
-  for tag in ec2.tags:
-      if tag['Key'] == 'Name':
-          print("Instance ID: %s, Instance Name:%s" % (ec2.id,tag['Value']))
-          ec2_to_bootup.append(ec2.id)
-
-if bootup:
-    print("Booting up these instances: %s)" % ec2_to_bootup)
-    for ec2ids in ec2_to_bootup:
-      ec2.instances.filter(InstanceIds = ec2ids).start()
-else:
-   print("Skipping bootup, set 'bootup = True' in the script to execute the shutdown!")
+    print("Listing all running instances for %s tag..." % openshift_tag)
+    for ec2inst in instances:
+      for tag in ec2inst.tags:
+        if tag['Key'] == 'Name':
+          print("Instance ID: %s, Instance Name:%s" % (ec2inst.id,tag['Value']))
+          ec2_to_bootup.append(ec2inst.id)
+    
+    if bootup:
+      print("Starting up these instances: %s)" % ec2_to_bootup)
+      #for ec2ids in ec2_to_bootup:
+      # ec2.instances.filter(InstanceIds = ec2ids).start()
+      ec2.instances.filter(InstanceIds = ec2_to_bootup).start()
+      return {
+            'statusCode': 200,
+            'body': json.dumps('%s started' % ec2_to_bootup)
+        }
+    else:
+       print("Skipping starting, set 'bootup = True' in the script to execute the startup!")
+       return {
+        'statusCode': 200,
+        'body': json.dumps('Bootup is false, skipped.')
+       }
--- aws/lambda/stop-ec2.py
+++ aws/lambda/stop-ec2.py
@@ -1,35 +1,42 @@
-#!/usr/bin/python3
-
+import json
 import boto3
 
-openshift_tag = "tag:kubernetes.io/cluster/$cluster_id_here"
+openshift_tag = "tag:kubernetes.io/cluster/XXXXXXX"
 region = "ap-southeast-1"
 state = "running"
-shutdown = False
+shutdown = True
 ec2_to_shutdown = []
 
-session =  boto3.Session(region_name=region)
-
-ec2 = session.resource('ec2', region)
-
-instances = ec2.instances.filter(
+def lambda_handler(event, context):
+    session = boto3.Session(region_name=region)
+    
+    ec2 = session.resource('ec2', region)
+    
+    instances = ec2.instances.filter(
           Filters=[
             {'Name': 'instance-state-name', 
              'Values': [state]},
             {'Name': openshift_tag,
              'Values': ['owned']}]
             )
-print("Listing all running instances for %s tag..." % openshift_tag) 
-
-for ec2 in instances:
-  for tag in ec2.tags:
-      if tag['Key'] == 'Name':
-          print("Instance ID: %s, Instance Name:%s" % (ec2.id,tag['Value']))
-          ec2_to_shutdown.append(ec2.id)
-
-if shutdown:
-    print("Shutting down these instances: %s)" % ec2_to_shutdown)
-    for ec2ids in ec2_to_shutdown:
-      ec2.instances.filter(InstanceIds = ec2ids).stop()
-else:
-   print("Skipping shutdown, set 'shutdown = True' in the script to execute the shutdown!")
+    print("Listing all running instances for %s tag..." % openshift_tag)
+    for ec2inst in instances:
+      for tag in ec2inst.tags:
+        if tag['Key'] == 'Name':
+          print("Instance ID: %s, Instance Name:%s" % (ec2inst.id,tag['Value']))
+          ec2_to_shutdown.append(ec2inst.id)
+    
+    if shutdown:
+      print("Shutting down these instances: %s)" % ec2_to_shutdown)
+      #for ec2ids in ec2_to_shutdown:
+      ec2.instances.filter(InstanceIds = ec2_to_shutdown).stop()
+      return {
+            'statusCode': 200,
+            'body': json.dumps('%s stopped' % ec2_to_shutdown)
+        }
+    else:
+       print("Skipping shutdown, set 'shutdown = True' in the script to execute the shutdown!")
+       return {
+        'statusCode': 200,
+        'body': json.dumps('Shutdown is false, skipped.')
+       }
