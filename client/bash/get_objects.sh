2963e24416735560697269de6a1e6dce9b7a1177
Muhammad Aizuddin <aizuddin.zali@gmail.com>
2022-06-30 17:30:52 +0800

    add bash collector script

---
+++ client/bash/get_objects.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+nameSpace=()
+k8sObject=("project" "deployment" "deploymentconfig" "statefulset"  
+           "persistentvolumeclaim" "secrets" "serviceaccount" 
+           "role" "rolebinding" "imagestream" "imagestreamtag"
+           "configmap" "daemonset" "cronjob" "job" "replicaset" "replicationcontroller"
+           "horizontalpodautoscaler" "pods" )
+
+filePrefix=$(date +%Y%m%d%s)
+
+for i in `oc get projects | egrep -v "^NAME" | cut -d " " -f 1 | egrep -v "^default|^kube|^openshift" `;
+  do
+    ns+=($i)
+done
+mkdir openshift-$filePrefix
+
+[ ! -d "openshift-$filePrefix" ] && echo "Unable to create folder!" && exit 1
+
+oc get pv -o yaml > openshift-$filePrefix/pv.yaml
+
+for namespace in ${ns[@]};
+  do
+    mkdir openshift-$filePrefix/$namespace
+    [ ! -d "openshift-$filePrefix/$namespace" ] && echo "Unable to create folder!" && exit 1
+
+    for object in ${k8sObject[@]};
+      do
+        mkdir openshift-$filePrefix/$namespace/$object
+        [ ! -d "openshift-$filePrefix/$namespace/$object" ] && echo "Unable to create folder!" && exit
+        echo "Collecting for $object in $namespace..."
+        oc get $object -o yaml -n $namespace >  openshift-$filePrefix/$namespace/$object/$object.yaml
+    done
+done 
+
+echo "Please tar.gz openshift-$filePrefix and send this to us..."
